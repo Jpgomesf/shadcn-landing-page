a0c0734f771c8c50a5884dc99f84f9b854727233
jpgomesf <jppromus@gmail.com>
2024-07-27 15:23:07 -0300

    :tada: begins project

---
+++ src/components/HeroCards.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { ChatLayout } from "./chat/chat-layout";
+import { parseCookies } from "nookies";
+
+export const HeroCards = () => {
+  const [defaultLayout, setDefaultLayout] = useState();
+
+  useEffect(() => {
+    const cookies = parseCookies();
+    const layout = cookies["react-resizable-panels:layout"];
+    if (layout) {
+      setDefaultLayout(JSON.parse(layout));
+    }
+  }, []);
+
+  return (
+    <div className="z-10 border rounded-lg max-w-5xl w-full h-full text-sm lg:flex">
+      <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} />
+    </div>
+  );
+};
---
+++ src/components/chat/chat.tsx
@@ -0,0 +1,33 @@
+import { Message, UserData } from "../../data/data";
+import ChatTopbar from "./chat-topbar";
+import { ChatList } from "./chat-list";
+import React from "react";
+
+interface ChatProps {
+  messages?: Message[];
+  selectedUser: UserData;
+  isMobile: boolean;
+}
+
+export function Chat({ messages, selectedUser, isMobile = true }: ChatProps) {
+  const [messagesState, setMessages] = React.useState<Message[]>(
+    messages ?? []
+  );
+
+  const sendMessage = (newMessage: Message) => {
+    setMessages([...messagesState, newMessage]);
+  };
+
+  return (
+    <div className="flex flex-col justify-between w-full h-[600px]">
+      <ChatTopbar selectedUser={selectedUser} />
+
+      <ChatList
+        messages={messagesState}
+        selectedUser={selectedUser}
+        sendMessage={sendMessage}
+        isMobile={isMobile}
+      />
+    </div>
+  );
+}
